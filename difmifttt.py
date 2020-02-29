797cf3bb54972a3d37f19cd591f722394c45eda9
Jeremy Fields <advanced4@gmail.com>
2020-02-29 14:39:43 -0500

    google assistant has unpredictable outputs for numbers

--- difmifttt.py
+++ difmifttt.py
@@ -108,6 +108,45 @@ def set_volume(device, volume):
     cast.set_volume(float(volume))
 
 
+def text2int(textnum):
+    numwords = {}
+    units = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+    "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+    "sixteen", "seventeen", "eighteen", "nineteen",
+    ]
+
+    tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+    scales = ["hundred", "thousand", "million", "billion", "trillion"]
+
+    numwords["and"] = (1, 0)
+    for idx, word in enumerate(units):    numwords[word] = (1, idx)
+    for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
+    for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
+
+    current = result = 0
+    for word in textnum.split():
+        if word not in numwords:
+          raise Exception("Illegal word: " + word)
+
+        scale, increment = numwords[word]
+        current = current * scale + increment
+        if scale > 100:
+            result += current
+            current = 0
+
+    return result + current
+
+
+def RepresentsInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
 @app.route('/api/<command>', methods=['POST'])
 def process(command):
     req = request.json
@@ -150,6 +189,15 @@ def process(command):
             volume = req['volume']
         else:
             volume = default_volume
+
+        if(isinstance(volume, str) ):  # google assistant will give us "five" instead of 5, but give you "25" if you ask for 25. wtf
+            if(RepresentsInt(volume)):
+                volume = int(volume)
+            else:
+                volume = text2int(volume)
+
+        if volume > 1:
+            volume = volume/100.0  # if it comes from google assistant, it will be "25"
         set_volume(device, volume)
         return jsonify(OK_RES), 200
 
